ac19b513361d5016c63a2d912644b4af92d07e87
Jiří Janoušek <janousek.jiri@gmail.com>
2016-09-21 10:44:35 +0200

    Move fix for broken AudioContext detection to top
    InitWebWorker is called too late in NP 3.0.3+ for this hack.
    
    Issue: tiliado/nuvola-app-yandex-music#3
    
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- integrate.js
+++ integrate.js
@@ -27,6 +27,16 @@
 (function(Nuvola)
 {
 
+if (Nuvola.global === window)
+{
+    /* Fix for broken detection of window.webkitAudioContext */
+    var origHasOwnProperty = window.hasOwnProperty;
+    window.hasOwnProperty = function(key)
+    {
+        return origHasOwnProperty.call(window, key) && window[key] !== undefined;
+    };
+}
+
 // Create media player component
 var player = Nuvola.$object(Nuvola.MediaPlayer);
 
@@ -42,12 +52,6 @@ WebApp._onInitWebWorker = function(emitter)
 {
     Nuvola.WebApp._onInitWebWorker.call(this, emitter);
     
-    /* Fix for broken detection of window.webkitAudioContext */
-    var origHasOwnProperty = window.hasOwnProperty;
-    window.hasOwnProperty = function(key)
-    {
-        return origHasOwnProperty.call(window, key) && window[key] !== undefined;
-    };
     
     var state = document.readyState;
     if (state === "interactive" || state === "complete")
