b0cf613325c3e2f57b5879b6f8d8476659a92a89
Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
2018-06-16 09:22:08 +0700

    Implement #14: Semi-automatic changes
    Signed-off-by: Aleksey Zhidkov <aleksey.zhidkov@gmail.com>

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Yandex Music (a Nuvola web app)
-============================
+Yandex Music • Nuvola Apps Runtime
+=================================
 
 Integration of Yandex Music into your linux desktop via
 [Nuvola Apps Runtime](https://github.com/tiliado/nuvolaruntime).
@@ -7,29 +7,38 @@ Integration of Yandex Music into your linux desktop via
 Support
 -------
 
-Report bugs and issues at <https://github.com/tiliado/nuvola-app-yandex-music/issues>.
+Follow [Bug reporting guidelines](https://github.com/tiliado/nuvolaruntime/wiki/Bug-Reporting-Guidelines).
 
 Installation
 ------------
+
+Install the [genuine cross-distribution flatpak packages](https://nuvola.tiliado.eu/app/yandex_music/).
+
+Building
+--------
 
 This project uses [Nuvola SDK](https://github.com/tiliado/nuvolasdk#create-new-project). Please refer to
 the [Build a Project Using Nuvola SDK](https://github.com/tiliado/nuvolasdk#build-a-project-using-nuvola-sdk)
 page in order to obtain information about dependencies and installation instructions.
 
+Change log
+----------
+
+See [CHANGELOG.md](./CHANGELOG.md).
+
+Contributing
+------------
+
+See [CONTRIBUTING.md](./CONTRIBUTING.md).
+
 Copyright
 ---------
 
   - `src/icon*.svg`
-    + Copyright 2015 Alexander King <alexanderddking@gmail.com >
-    + Copyright 2016 Jiří Janoušek <janousek.jiri@gmail.com>
-    + License: [2-Clause BSD-license](./LICENSE)
-    
-  - integrate.js:
-    + Copyright 2017 Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
-    + Copyright 2016-2017 Jiří Janoušek <janousek.jiri@gmail.com>
-    + Copyright 2016 Basil Belokon <basil.belokon@gmail.com>
-    + License: [2-Clause BSD-license](./LICENSE-BSD.txt)
+    + Copyright 2011 Alexander King <alexanderddking@gmail.com>
+    + Copyright 2011 Arturo Torres Sánchez <arturo.ilhuitemoc@gmail.com>
+    + License: [CC-BY-3.0](./LICENSE-CC-BY.txt)
 
   - Other files:
-    + Copyright 2016 Jiří Janoušek <janousek.jiri@gmail.com>
+    + Copyright 2018 Aleksey Zhidkov <None>
     + License: [2-Clause BSD-license](./LICENSE-BSD.txt)
--- integrate.js
+++ integrate.js
@@ -3,14 +3,14 @@
  * Copyright 2017 Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
  *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met: 
- * 
+ * modification, are permitted provided that the following conditions are met:
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer. 
+ *    list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution. 
- * 
+ *    and/or other materials provided with the distribution.
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -23,172 +23,165 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-"use strict";
+'use strict';
 
-(function(Nuvola)
-{
-
-if (Nuvola.global === window)
-{
+(function (Nuvola) {
+  if (Nuvola.global === window) {
     /* Fix for broken detection of window.webkitAudioContext */
-    var origHasOwnProperty = window.hasOwnProperty;
-    window.hasOwnProperty = function(key)
-    {
-        return origHasOwnProperty.call(window, key) && window[key] !== undefined;
-    };
-}
+    var origHasOwnProperty = window.hasOwnProperty
+    window.hasOwnProperty = function (key) {
+      return origHasOwnProperty.call(window, key) && window[key] !== undefined
+    }
+  }
 
 // Create media player component
-var player = Nuvola.$object(Nuvola.MediaPlayer);
+  var player = Nuvola.$object(Nuvola.MediaPlayer)
 
 // Handy aliases
-var PlaybackState = Nuvola.PlaybackState;
-var PlayerAction = Nuvola.PlayerAction;
+  var PlaybackState = Nuvola.PlaybackState
+  var PlayerAction = Nuvola.PlayerAction
 
 // Create new WebApp prototype
-var WebApp = Nuvola.$WebApp();
+  var WebApp = Nuvola.$WebApp()
 
 // Translations
-var C_ = Nuvola.Translate.pgettext;
+  var C_ = Nuvola.Translate.pgettext
 
-var ACTION_LIKE = "like";
+  var ACTION_LIKE = 'like'
 
 // Initialization routines
-WebApp._onInitWebWorker = function(emitter)
-{
-    Nuvola.WebApp._onInitWebWorker.call(this, emitter);
+  WebApp._onInitWebWorker = function (emitter) {
+    Nuvola.WebApp._onInitWebWorker.call(this, emitter)
 
-    var state = document.readyState;
-    if (state === "interactive" || state === "complete")
-        this._onPageReady();
-    else
-        document.addEventListener("DOMContentLoaded", this._onPageReady.bind(this));
+    var state = document.readyState
+    if (state === 'interactive' || state === 'complete') {
+      this._onPageReady()
+    } else {
+      document.addEventListener('DOMContentLoaded', this._onPageReady.bind(this))
+    }
 
-    Nuvola.actions.addAction("like", "win", ACTION_LIKE, C_("Action", "Like"),
-        null, null, null, true);
-}
+    Nuvola.actions.addAction('like', 'win', ACTION_LIKE, C_('Action', 'Like'),
+        null, null, null, true)
+  }
 
 // Page is ready for magic
-WebApp._onPageReady = function()
-{
+  WebApp._onPageReady = function () {
     // Connect handler for signal ActionActivated
-    Nuvola.actions.connect("ActionActivated", this);
-    try { 
-        // Remove advertising sidebar
-        document.querySelector(".centerblock-wrapper").setAttribute('style', 'width: 100%');
-        var sidebar = document.querySelector(".sidebar__placeholder .sidebar");
-        sidebar.parentNode.removeChild(sidebar);
-    } catch (e){ /* ignored */ }
+    Nuvola.actions.connect('ActionActivated', this)
+    try {
+      // Remove advertising sidebar
+      document.querySelector('.centerblock-wrapper').setAttribute('style', 'width: 100%')
+      var sidebar = document.querySelector('.sidebar__placeholder .sidebar')
+      sidebar.parentNode.removeChild(sidebar)
+    } catch (e) { /* ignored */ }
 
-    player.addExtraActions([ACTION_LIKE]);
+    player.addExtraActions([ACTION_LIKE])
 
     // Start update routine
-    this.update();
-}
+    this.update()
+  }
 
 // Extract data from the web page
-WebApp.update = function()
-{
+  WebApp.update = function () {
     var track = {
-        title: null,
-        artist: null,
-        album: null,
-        artLocation: null
+      title: null,
+      artist: null,
+      album: null,
+      artLocation: null
     }
-    
-    try
-    {
-        track.title = document.querySelector(".player-controls .track .track__title").innerText;
-        track.artist = document.querySelector(".player-controls .track .track__artists").innerText;
-        track.artLocation = document.querySelector(".player-controls .track .track-cover").src.replace(
-            /\d+x\d+$/, "200x200");
-    }
-    catch (e)
-    {
-        //~ console.log(e);
-    }
-    
-    player.setTrack(track);
-    
-    var buttons = this.getButtons();
-    if (buttons.pause)
-        var state = PlaybackState.PLAYING;
-    else if (buttons.play)
-        var state = PlaybackState.PAUSED;
-    else
-        var state = PlaybackState.UNKNOWN;
-    player.setPlaybackState(state);
-    
-    player.setCanGoPrev(!!buttons.prev);
-    player.setCanGoNext(!!buttons.next);
-    player.setCanPlay(!!buttons.play);
-    player.setCanPause(!!buttons.pause);
 
-    var actionsEnabled = {};
-    var actionsStates = {};
-    actionsEnabled[ACTION_LIKE] = buttons.like != null && document.querySelector(".head__userpic") != null;
-    actionsStates[ACTION_LIKE] = buttons.like != null && document.querySelector(".icon_like_on") != null;
-    Nuvola.actions.updateEnabledFlags(actionsEnabled);
-    Nuvola.actions.updateStates(actionsStates);
+    try {
+      track.title = document.querySelector('.player-controls .track .track__title').innerText
+      track.artist = document.querySelector('.player-controls .track .track__artists').innerText
+      track.artLocation = document.querySelector('.player-controls .track .track-cover').src.replace(
+            /\d+x\d+$/, '200x200')
+    } catch (e) {
+        // ~ console.log(e);
+    }
+
+    player.setTrack(track)
+
+    var buttons = this.getButtons()
+    var state = null
+    if (buttons.pause) {
+      state = PlaybackState.PLAYING
+    } else if (buttons.play) {
+      state = PlaybackState.PAUSED
+    } else {
+      state = PlaybackState.UNKNOWN
+    }
+    player.setPlaybackState(state)
+
+    player.setCanGoPrev(!!buttons.prev)
+    player.setCanGoNext(!!buttons.next)
+    player.setCanPlay(!!buttons.play)
+    player.setCanPause(!!buttons.pause)
+
+    var actionsEnabled = {}
+    var actionsStates = {}
+    actionsEnabled[ACTION_LIKE] = buttons.like != null && document.querySelector('.head__userpic') != null
+    actionsStates[ACTION_LIKE] = buttons.like != null && document.querySelector('.icon_like_on') != null
+    Nuvola.actions.updateEnabledFlags(actionsEnabled)
+    Nuvola.actions.updateStates(actionsStates)
 
     // Schedule the next update
-    setTimeout(this.update.bind(this), 500);
-}
+    setTimeout(this.update.bind(this), 500)
+  }
 
-
-WebApp.getButtons = function()
-{
-    var notDisabled = function(selector)
-    {
-        var elm = document.querySelector(selector);
-        return (elm && !elm.classList.contains("player-controls__btn_disabled")) ? elm : null;
+  WebApp.getButtons = function () {
+    var notDisabled = function (selector) {
+      var elm = document.querySelector(selector)
+      return (elm && !elm.classList.contains('player-controls__btn_disabled')) ? elm : null
     }
-    var playPause = notDisabled(".player-controls__btn_play");
+    var playPause = notDisabled('.player-controls__btn_play')
     return {
-        prev: notDisabled(".player-controls__btn_prev"),
-        play: playPause && playPause.classList.contains("player-controls__btn_pause") ? null : playPause,
-        pause: playPause && playPause.classList.contains("player-controls__btn_pause") ? playPause : null,
-        next: notDisabled(".player-controls__btn_next"),
-        like: notDisabled(".player-controls__btn.like_player")
+      prev: notDisabled('.player-controls__btn_prev'),
+      play: playPause && playPause.classList.contains('player-controls__btn_pause') ? null : playPause,
+      pause: playPause && playPause.classList.contains('player-controls__btn_pause') ? playPause : null,
+      next: notDisabled('.player-controls__btn_next'),
+      like: notDisabled('.player-controls__btn.like_player')
     }
-}
+  }
 
 // Handler of playback actions
-WebApp._onActionActivated = function(emitter, name, param)
-{
-    var buttons = this.getButtons();
-    switch (name)
-    {
-    case PlayerAction.TOGGLE_PLAY:
-        if (buttons.play)
-            Nuvola.clickOnElement(buttons.play);
-        else if (buttons.pause)
-            Nuvola.clickOnElement(buttons.pause);
-        break;
-    case PlayerAction.PLAY:
-        if (buttons.play)
-            Nuvola.clickOnElement(buttons.play);
-        break;
-    case PlayerAction.PAUSE:
-    case PlayerAction.STOP:
-        if (buttons.pause)
-            Nuvola.clickOnElement(buttons.pause);
-        break;
-    case PlayerAction.PREV_SONG:
-        if (buttons.prev)
-            Nuvola.clickOnElement(buttons.prev);
-        break;
-    case PlayerAction.NEXT_SONG:
-        if (buttons.next)
-            Nuvola.clickOnElement(buttons.next);
-        break;
-    case ACTION_LIKE:
-        if (buttons.like)
-          Nuvola.clickOnElement(buttons.like);
-        break;
+  WebApp._onActionActivated = function (emitter, name, param) {
+    var buttons = this.getButtons()
+    switch (name) {
+      case PlayerAction.TOGGLE_PLAY:
+        if (buttons.play) {
+          Nuvola.clickOnElement(buttons.play)
+        } else if (buttons.pause) {
+          Nuvola.clickOnElement(buttons.pause)
+        }
+        break
+      case PlayerAction.PLAY:
+        if (buttons.play) {
+          Nuvola.clickOnElement(buttons.play)
+        }
+        break
+      case PlayerAction.PAUSE:
+      case PlayerAction.STOP:
+        if (buttons.pause) {
+          Nuvola.clickOnElement(buttons.pause)
+        }
+        break
+      case PlayerAction.PREV_SONG:
+        if (buttons.prev) {
+          Nuvola.clickOnElement(buttons.prev)
+        }
+        break
+      case PlayerAction.NEXT_SONG:
+        if (buttons.next) {
+          Nuvola.clickOnElement(buttons.next)
+        }
+        break
+      case ACTION_LIKE:
+        if (buttons.like) {
+          Nuvola.clickOnElement(buttons.like)
+        }
+        break
     }
-}
+  }
 
-WebApp.start();
-
-})(this);  // function(Nuvola)
+  WebApp.start()
+})(this)  // function(Nuvola)
